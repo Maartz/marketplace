e5b34b92222a4d4973d761ce6d5b2a9cece1b520
Maartz <maartz@MacBook-Pro-de-Maartz.local>
2020-03-26 14:52:04 +0100

    Create admin? and author_of? helpers | Update project index layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def author_of(resource)
+        # Check if the user is signed in and if the resource id passed is the same as the current user ones
+        user_signed_in? && resource.user_id == current_user.id
+    end
+
+    def admin?
+        user_signed_in? && current_user.admin?
+    end
 end
